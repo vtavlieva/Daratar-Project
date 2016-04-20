963c0d8491168ac843c4da875812d7615cd457a7
radiberry <radiberry@mail.bg>
2016-04-20 21:56:22 +0300

    Adding test to cover login with invalid credentials.

--- DaratarProjectAutomatedTests/src/test/java/DaratarProjectAutomatedTestLoginFunctionality.java
+++ DaratarProjectAutomatedTests/src/test/java/DaratarProjectAutomatedTestLoginFunctionality.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class DaratarProjectAutomatedTestLoginFunctionality {
     private WebDriver driver;
@@ -42,6 +43,30 @@ public class DaratarProjectAutomatedTestLoginFunctionality {
         WebElement welcomeInfoLoginScreen = driver.findElement(By.id("logInfo"));
         assertEquals("http://pdmtrades.eu/index.php/Library/index", driver.getCurrentUrl());
         assertEquals("Welcome, login! Log out", welcomeInfoLoginScreen.getText());
+    }
+    @Test
+    public void TestLogin_ShouldLoginUnsuccessfully() {
+        driver.get("http://pdmtrades.eu/");
+
+        String invalidUsername = "login1";
+        String invalidPassword = "1234";
+
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+
+        usernameField.clear();
+        usernameField.sendKeys(invalidUsername);
+
+        passwordField.clear();
+        passwordField.sendKeys(invalidPassword);
+
+        WebElement loginButton = driver.findElement(By.id("login-submit"));
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        WebElement failToLoginErrorMessage = driver.findElement(By.id("user-result"));
+        assertEquals("http://pdmtrades.eu/", driver.getCurrentUrl());
+        assertEquals("You've entered invalid data, please try again!", failToLoginErrorMessage.getText());
     }
 
     @After
